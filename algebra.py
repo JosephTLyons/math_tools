5b25d31af5a975dd5087193bac601e8800936801
Joseph Lyons <josephtlyons@gmail.com>
2019-10-15 21:52:16 -0400

    Move algebra related items to their own file

---
+++ algebra.py
@@ -0,0 +1,36 @@
+from math_tools import *
+
+
+def quadratic(a, b, c):
+    if a == 0:
+        raise ValueError("Not a quadratic function")
+
+    preliminary = (b ** 2) - (4 * a * c)
+
+    if preliminary < 0:
+        return []
+
+    common_1 = math.sqrt(preliminary)
+    common_2 = 2 * a
+
+    x_1 = (-b + common_1) / common_2
+    x_2 = (-b - common_1) / common_2
+
+    if x_1 == x_2:
+        return [x_1]
+
+    return sorted([x_1, x_2])
+
+
+def intersection(a_1, b_1, a_2, b_2):
+    if a_1 == a_2:
+        if b_1 == b_2:
+            raise ValueError("Lines are equal")
+
+        else:
+            raise ValueError("The lines do not intersect")
+
+    x = (b_2 - b_1) / (a_1 - a_2)
+    y = (a_1 * x) + b_1
+
+    return (x, y)
---
+++ test_algebra.py
@@ -0,0 +1,47 @@
+import pytest
+from algebra import *
+
+
+class TestQuadratic:
+    def test_has_x_intercepts_1(self):
+        # -2x^2 + x + 3
+        assert [-1.0, 1.5] == quadratic(-2, 1, 3)
+
+    def test_has_x_intercepts_2(self):
+        # x^2 - 4
+        assert [-2.0, 2.0] == quadratic(1, 0, -4)
+
+    def test_has_x_intercepts_3(self):
+        # 3x^2 + 3x
+        assert [-1.0, 0] == quadratic(3, 3, 0)
+
+    def test_has_one_x_intercept_1(self):
+        # x^2
+        assert [0] == quadratic(1, 0, 0)
+
+    def test_has_one_x_intercept_2(self):
+        # -x^2
+        assert [0] == quadratic(-1, 0, 0)
+
+    def test_has_no_x_intercepts_1(self):
+        # x^2 + 1 : no X intercepts
+        assert [] == quadratic(1, 0, 1)
+
+    def test_has_no_x_intercepts_2(self):
+        # -x^2 - 1 : no X intercepts
+        assert [] == quadratic(-1, 0, -1)
+
+    def test_is_not_a_quadratic_function(self):
+        # 0x^2 + 1x + 0 : isn't a quadratic
+        with pytest.raises(ValueError):
+            quadratic(0, 1, 0)
+
+
+class TestIntersection:
+    def test_for_intersection(self):
+        assert intersection(2, 1, 3, 1) == (0, 1)
+        assert intersection(-3, 3, (1 / 5), 3) == (0, 3)
+
+    def test_for_no_intersection(self):
+        with pytest.raises(ValueError):
+            intersection(0, 1, 0, 2)
